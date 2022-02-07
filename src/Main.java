d2efa45f1aaad4a8ae972a6615df16b7b7da85f0
fahrimaulana <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2022-02-07 08:47:02 +0700

    sudah selesai

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        kelulusan abby = new kelulusan();
+        kelulusan abizar = new kelulusan();
+        System.out.println("Masukkan nomor absen anda: ");
+        int absensi = sc.nextInt();
+
+        switch (absensi){
+            case 1:
+                kelulusan.abby();
+                break;
+            case 2:
+                kelulusan.abizar.abizar();
+        }
     }
 }
--- src/kelulusan.java
+++ src/kelulusan.java
@@ -1,5 +1,32 @@
 public class kelulusan {
-    public static void main(String[] args) {
-        
+    static String nama = "Abby Satya Diananda";
+    static int nilai = 70, absen = 1, nis = 16546;
+
+    public static void abby(){
+        System.out.println("Nama: "+nama);
+        System.out.println("No Absen: "+absen);
+        System.out.println("NIS: "+nis);
+        System.out.println("Nilai produktif "+nama+" "+nilai);
+
+        if (nilai >= 70){
+            System.out.println("Maaf "+nama+" anda tidak lulus");
+        }
+    }
+    class abizar{
+        static String nama = "Abizar Nazha";
+        static int nilai = 75, absen = 2, nis = 58135;
+
+        public static void abizar(){
+            System.out.println("Nama: "+nama);
+            System.out.println("No Absen: "+absen);
+            System.out.println("NIS: "+nis);
+            System.out.println("Nilai Produktif "+nama+" "+nilai);
+
+            if (nilai >= 70){
+                System.out.println("Maaf "+nama+" anda tidak lulus");
+            }else{
+                System.out.println("Selamat "+nama+ "anda lulus");
+            }
+        }
     }
 }
