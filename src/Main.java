292fffb628ba055eecc33b50239193fecd85deb0
fahrimaulana <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2022-02-07 22:11:06 +0700

    Commit kedua ya adik adik, belum selesai juga

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,19 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
-        //Method
-        kelulusan abby = new kelulusan();
-        kelulusan abizar = new kelulusan();
-        kelulusan nabil = new kelulusan();
-        kelulusan reza = new kelulusan();
+        //Method setiap siswa
+        kelulusan abby = new kelulusan();//1
+        kelulusan abizar = new kelulusan();//2
+        kelulusan nabil = new kelulusan();//3
+        kelulusan reza = new kelulusan();//4
+        kelulusan adrian = new kelulusan();//5
+        kelulusan farhan = new kelulusan();//6
+        kelulusan tama = new kelulusan();//7
+        kelulusan arumi = new kelulusan();//8
+        kelulusan aurel = new kelulusan();//9
+        kelulusan avifta = new kelulusan();//10
+        kelulusan camel = new kelulusan();//11
+        kelulusan karel = new kelulusan();//12
 
         System.out.println("Masukkan nomor absen anda: ");
         int absensi = sc.nextInt();
@@ -25,6 +33,30 @@ public class Main {
                 break;
             case 4:
                 kelulusan.reza.Reza();
+                break;
+            case 5:
+                kelulusan.adrian.adrian();
+                break;
+            case 6:
+                kelulusan.farhan.farhan();
+                break;
+            case 7:
+                kelulusan.tama.tama();
+                break;
+            case 8:
+                kelulusan.arumi.arumi();
+                break;
+            case 9:
+                kelulusan.aurel.aurel();
+                break;
+            case 10:
+                kelulusan.avifta.avifta();
+                break;
+            case 11:
+                kelulusan.camel.camel();
+                break;
+            case 12:
+                kelulusan.carolina.karel();
                 break;
         }
     }
--- src/kelulusan.java
+++ src/kelulusan.java
@@ -12,6 +12,7 @@ public class kelulusan {
             System.out.println("Maaf "+nama+" anda tidak lulus");
         }
     }
+
     class abizar{
         static String nama = "Abizar Nazha";
         static int nilai = 75, absen = 2, nis = 58135;
@@ -29,6 +30,7 @@ public class kelulusan {
             }
         }
     }
+
     class nabil{
         static String nama = "Achmad Nabil Afgareza";
         static int nilai = 76, absen = 3, nis = 67033;
@@ -46,6 +48,7 @@ public class kelulusan {
             }
         }
     }
+
     class reza{
         static String nama = "Achmad Reza Subkhan";
         static int nilai = 80, absen = 4, nis = 54403;
@@ -60,6 +63,168 @@ public class kelulusan {
                 System.out.println("Maaf "+nama+" anda tidak lulus");
             }else{
                 System.out.println("Selamat "+nama+ " anda lulus");
+            }
+        }
+    }
+
+    class adrian{
+        static String nama = "Adrian Pramana Wijaya";
+        static int nilai = 77, absen = 5, nis = 42003;
+
+        public static void adrian(){
+            System.out.println("Nama: "+nama);
+            System.out.println("No Absen: "+absen);
+            System.out.println("NIS: "+nis);
+            System.out.println("Nilai produktif "+nama+" "+nilai);
+
+            if (nilai <= 70){
+                System.out.println("Maaf "+nama+" anda tidak lulus");
+            }else{
+                System.out.println("Selamat "+nama+ " anda lulus");
+            }
+        }
+    }
+
+    class farhan{
+        static String nama = "Ahmad Shah Farhan";
+        static int nilai = 70, absen = 6, nis = 48324;
+
+        public static void farhan(){
+            System.out.println("Nama: "+nama);
+            System.out.println("No Absen: "+absen);
+            System.out.println("NIS: "+nis);
+            System.out.println("Nilai produktif "+nama+" "+nilai);
+
+            if (nilai <= 70) {
+                System.out.println("Maaf "+nama+" anda tidak lulus");
+            }else{
+                System.out.println("Selamat "+nama+" anda lulus");
+            }
+        }
+    }
+
+    class tama{
+        static String nama = "Ali Agung Pratama";
+        static int nilai = 72, absen = 7, nis = 4400;
+
+        public static void tama(){
+            System.out.println("Nama: "+nama);
+            System.out.println("No Absen: "+absen);
+            System.out.println("NIS: "+nis);
+            System.out.println("Nilai produktif "+nama+" "+nilai);
+
+            if (nilai <=70 ){
+                System.out.println("Maaf "+nama+" anda tidak lulus");
+            }else{
+                System.out.println("Selamat "+nama+" anda lulus");
+            }
+        }
+    }
+
+    class arumi{
+        static String nama = "Arumi Firja Windraya";
+        static int nilai = 69, absen = 8, nis = 5134;
+
+        public static void arumi(){
+            System.out.println("Nama: "+nama);
+            System.out.println("No Absen: "+absen);
+            System.out.println("NIS: "+nis);
+            System.out.println("Nilai produktif "+nama+" "+nilai);
+
+            if (nilai <=70 ){
+                System.out.println("Maaf "+nama+" anda tidak lulus");
+            }else{
+                System.out.println("Selamat "+nama+" anda lulus");
+            }
+        }
+    }
+
+    class aurel{
+        static String nama = "Aurelita Rechan Salsabil";
+        static int nilai = 78, absen = 9, nis = 1384;
+
+        public static void aurel(){
+            System.out.println("Nama: "+nama);
+            System.out.println("No Absen: "+absen);
+            System.out.println("NIS: "+nis);
+            System.out.println("Nilai produktif "+nama+" "+nilai);
+
+            if (nilai <=70 ){
+                System.out.println("Maaf "+nama+" anda tidak lulus");
+            }else{
+                System.out.println("Selamat "+nama+" anda lulus");
+            }
+        }
+    }
+
+    class avifta{
+        static String nama = "Avifta Yulya Rismawa";
+        static int nilai = 84, absen = 10, nis = 2490;
+
+        public static void avifta(){
+            System.out.println("Nama: "+nama);
+            System.out.println("No Absen: "+absen);
+            System.out.println("NIS: "+nis);
+            System.out.println("Nilai produktif "+nama+" "+nilai);
+
+            if (nilai <=70 ){
+                System.out.println("Maaf "+nama+" anda tidak lulus");
+            }else{
+                System.out.println("Selamat "+nama+" anda lulus");
+            }
+        }
+    }
+
+    class camel{
+        static String nama = "Camelia Azzahra";
+        static int nilai = 65, absen = 11, nis = 1283;
+
+        public static void camel(){
+            System.out.println("Nama: "+nama);
+            System.out.println("No Absen: "+absen);
+            System.out.println("NIS: "+nis);
+            System.out.println("Nilai produktif "+nama+" "+nilai);
+
+            if (nilai <=70 ){
+                System.out.println("Maaf "+nama+" anda tidak lulus");
+            }else{
+                System.out.println("Selamat "+nama+" anda lulus");
+            }
+        }
+    }
+
+    class carolina{
+        static String nama = "Carolina Widya Putri Rahmanti";
+        static int nilai = 73, absen = 12, nis = 6248;
+
+        public static void karel(){
+            System.out.println("Nama: "+nama);
+            System.out.println("No Absen: "+absen);
+            System.out.println("NIS: "+nis);
+            System.out.println("Nilai produktif "+nama+" "+nilai);
+
+            if (nilai <=70 ){
+                System.out.println("Maaf "+nama+" anda tidak lulus");
+            }else{
+                System.out.println("Selamat "+nama+" anda lulus");
+            }
+        }
+    }
+
+    class cla{
+        static String nama = "Ali Agung Pratama";
+        static int nilai = 71, absen = 13, nis = 1384;
+
+        public static void cla(){
+            System.out.println("Nama: "+nama);
+            System.out.println("No Absen: "+absen);
+            System.out.println("NIS: "+nis);
+            System.out.println("Nilai produktif "+nama+" "+nilai);
+
+            if (nilai <=70 ){
+                System.out.println("Maaf "+nama+" anda tidak lulus");
+            }else{
+                System.out.println("Selamat "+nama+" anda lulus");
             }
         }
     }
